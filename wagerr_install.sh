c0f334a8f75cc63a70fa50b8df1fd4947011de92
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2018-09-18 15:19:55 -0500

    Update wagerr_install.sh

--- wagerr_install.sh
+++ wagerr_install.sh
@@ -124,17 +124,6 @@ maxconnections=256
 masternode=1
 externalip=$NODEIP:$COIN_PORT
 masternodeprivkey=$COINKEY
-addnode=45.64.99.142:22500
-addnode=207.246.88.39:22500
-addnode=107.175.92.137:22500
-addnode=95.179.133.95:22500
-addnode=77.55.217.45:22500
-addnode=149.28.111.33:22500
-addnode=45.76.234.97:22500
-addnode=80.211.10.194:22500
-addnode=107.148.16.238:22500
-addnode=23.101.130.219:22500
-addnode=45.32.237.176:22500
 EOF
 }
 
